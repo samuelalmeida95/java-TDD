83f503aa6fb7b15fa9e58edd4a6412958235dfc7
samuelalmeida95 <samuelalmeida95>
2021-05-13 11:11:49 -0300

    adicionando teste para empilhar e desempilhar na pilha

--- src/main/java/tdd/Pilha.java
+++ src/main/java/tdd/Pilha.java
@@ -2,11 +2,11 @@ package tdd;
 
 public class Pilha {
 
-    private Object elemento;
-    private int quantidade;
+    private Object [] elementos = new Object [10];
+    private int quantidade = 0;
 
     public boolean estaVazia() {
-        return (elemento == null); 
+        return (quantidade == 0); 
     }
 
     public Object tamanho() {
@@ -14,11 +14,17 @@ public class Pilha {
     }
 
     public void empilha(Object elemento) {
-        this.elemento = elemento;
+        this.elementos[quantidade] = elemento;
         quantidade ++;
     }
 
     public Object topo() {
-        return this.elemento;
+        return this.elementos[quantidade - 1];
+    }
+
+    public Object desempilha() {
+        Object topo = topo();
+        quantidade --;
+        return topo;
     }
 }
--- src/test/java/tdd/PilhaTest.java
+++ src/test/java/tdd/PilhaTest.java
@@ -24,6 +24,21 @@ public class PilhaTest {
         assertEquals("primeiro", p.topo());
     }
 
+    @Test
+    public void empilhaEDesempilha() {
+        Pilha p = new Pilha();
+        p.empilha("primeiro");
+        p.empilha("segundo");
+        assertEquals(2, p.tamanho());
+        assertEquals("segundo", p.topo());
+
+        Object desempilhado = p.desempilha();
+        assertEquals(1, p.tamanho());
+        assertEquals("primeiro", p.topo());
+        assertEquals("segundo", desempilhado);
+
+    }
+
 
     
 }
